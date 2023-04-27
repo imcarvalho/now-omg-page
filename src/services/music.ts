8495462b49f2b4a69776fe62f675075b4c6ac272
Inês Carvalho <4671109+imcarvalho@users.noreply.github.com>
2023-04-27 12:38:20 +0100

    Fix fail of lastfm periods on the gettopalbums method

--- src/services/music.ts
+++ src/services/music.ts
@@ -5,12 +5,12 @@ dotenv.config();
 
 const getURL = (
   method: 'gettopalbums' | 'gettoptracks' | 'gettopartists',
-  period: 7 | 30 = 30
+  period: "1month" | "7day" | "overall" = "1month"
 ) => {
   const apiKey = process.env.LASTFMKEY;
   const user = process.env.LASTFMUSER;
 
-  return `https://ws.audioscrobbler.com/2.0/?method=user.${method}&user=${user}&api_key=${apiKey}&format=json&period=${period}day`;
+  return `https://ws.audioscrobbler.com/2.0/?method=user.${method}&user=${user}&api_key=${apiKey}&format=json&period=${period}`;
 };
 
 const getImage = (images: { size: string; '#text': string }[]) => {
